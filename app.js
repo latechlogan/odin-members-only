6955b42642ac5bdf36da7bfb539661aceb822bff
Logan Baugh <7052181+latechlogan@users.noreply.github.com>
2026-01-19 23:14:11 -0600

    feat: add basic express server setup

---
+++ app.js
@@ -0,0 +1,13 @@
+const express = require("express");
+const app = express();
+
+const PORT = 3000;
+
+//middlewares
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+//routes
+app.get("/", (req, res) => res.send("homepage"));
+
+app.listen(PORT, () => console.log(`Application listening on port: ${PORT}`));
